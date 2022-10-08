38aad481e28fb3afa549ee1467ef2706a69e425c
joao <joao4729@gmail.com>
2022-10-08 17:18:47 -0300

    at

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
+import React from 'react'
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './componets/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
-import React, { Component } from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-              <>
-                 <Header />
-              </>
-            </div>
-        );
-    }
+
+const App = () => {
+
+    return (
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Portfolio />
+            <Testimonials />
+            <Contact />
+            <Footer />
+        </>
+    )
 }
 
 export default App;
+
