58e1589905d44aee4c6ddc0a03beda4ee638a574
joao <joao4729@gmail.com>
2022-10-20 08:34:05 -0300

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from'./CTA'
+import ME from '../../assents/me.png'
+
 
 const header = () => {
   return (
@@ -9,8 +11,14 @@ const header = () => {
         <h5>Hello I'm</h5>
         <h1>Hajia Bintu</h1>
         <h5 className="text-light">Fullstack Developer</h5>
-        
         <CTA />
+        
+        
+        <div className="me">
+          <img src={ME}alt="me" />
+        </div>
+
+          <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
