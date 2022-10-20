80e08563d0d2b60d9abfc8320801103af8c441bd
joao <joao4729@gmail.com>
2022-10-19 23:26:39 -0300

    atualização

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './header.css'
+import CTA from'./CTA'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Hajia Bintu</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        
+        <CTA />
+      </div>
+    </header>
   )
 }
 
