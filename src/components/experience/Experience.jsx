7e7351feae8cd6f03f3f5fb90bc4a453a80c02f0
joao <joao4729@gmail.com>
2022-10-25 18:48:33 -0300

    commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const experience = () => {
   return (
-    <section id='experience'>experience</section>
-  )
-}
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experiencia</h2>
 
-export default experience
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Javascript</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Tailwind</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>React</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+          </div>
+        </div>
+
+        {/*END OF FRONTEND*/}
+
+        <div className="experience__backend">
+          <h3>Bckeend Development</h3>
+
+          <article className="experience__details">
+            <BsPatchCheckFill />
+            <h4>Node JS</h4>
+            <small className="text-light">Intermediate</small>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill />
+            <h4>mongoDB</h4>
+            <small className="text-light">Intermediate</small>
+          </article>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>PHP</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>python</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default experience;
