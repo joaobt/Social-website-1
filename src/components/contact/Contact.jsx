54a809dd8dc7e5e073b2f893da64ec6f033740a7
joao <joao4729@gmail.com>
2022-10-27 22:23:04 -0300

    commit 29-10

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
-import './contact.css'
 
-const contact = () => {
-  return (
-    <section id='contact'>contact</section>
-  )
+import React from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
+import { useRef, } from "react";
+
+ const Contact =() => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+     
+        emailjs.sendForm(
+            "service_33jp94m",
+            "template_mr4kv6s",
+            form.current,
+            "user_SO4fmcQs-RyLpYi3Z"
+        ); // erro
+    };
+    return (
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option">
+                        <MdOutlineEmail className="contact__option-icon" />
+                        <h4>Email</h4>
+                        <h5>dummyegator@gmail.com</h5>
+                        <a href="joao4729@gmail.com" target="__blank">
+                            Send message
+                        </a>
+                    </article>
+
+                    <article className="contact__option">
+                        <RiMessengerLine className="contact__option-icon" />
+                        <h4>Messager</h4>
+                        <h5>egatortutorials</h5>
+                        <a href="http://m.me/ernest.achievier" target="__blank">
+                            Send message
+                        </a>
+                    </article>
+
+                    <article className="contact__option">
+                        <BsWhatsapp className="contact__option-icon" />
+                        <h4>WhatssApp</h4>
+                        <h5>+123456789</h5>
+                        <a href="https://web.whatsapp.com/" target="__blank">
+                            Send message
+                        </a>
+                    </article>
+                </div>
+                {/*=================================END CONTACT OPTION============*/}
+                <form ref={form} onSubmit={sendEmail}>
+                    
+                       <input type="text" name="name"  placeholder="Your Full Name"required />
+                       <input type="Email" name="email" placeholder="Your Email" required />
+                       <textarea name="message"rows="7"placeholder="Your Message"required></textarea>
+                       <button type="submit" className="btn btn-primary">Send Message</button>
+                </form>
+            </div>
+        </section>
+    );
 }
+export default Contact
 
-export default contact
+  
+
+
+                    
+                    
+                        
+                        
+                        
+                        
+                  
+                   
+                        
+                   
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,24 @@
-
 import React from "react";
 import "./testimonials.css";
 import AVTR1 from "../../assents/avatar1.jpg";
 import AVTR2 from "../../assents/avatar2.jpg";
 import AVTR3 from "../../assents/avatar3.jpg";
 import AVTR4 from "../../assents/avatar4.jpg";
+import {Pagination} from 'swiper';
+
+// import Swiper core and required modules
+//import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+
+
 
 
 const data = [
@@ -39,25 +53,38 @@ const data = [
 
 const testimonials = () => {
   return (
+    
     <section id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+            // install Swiper modules
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            pagination={{ clickable: true }}>
+           
         {data.map(({ avatar, name, review }, index) => {
           return (
-            <article key={index} className="testimonial">
+            <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar">
-                <img src={avatar} alt=''/>
+                <img src={avatar} alt="" />
               </div>
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
-            </article>
+              
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
+  
   );
 };
 
 export default testimonials;
+           
+     
+     
+        
