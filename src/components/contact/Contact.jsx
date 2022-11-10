2553a7edf94cc334a450e4d48c4ecf7ad116a8f8
joao <joao4729@gmail.com>
2022-11-10 17:53:41 -0300

    atualizar

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,42 +3,50 @@ import React from "react";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiMessengerLine } from "react-icons/ri";
 import { BsWhatsapp } from "react-icons/bs";
+import  { useRef } from 'react';
 import emailjs from "emailjs-com";
-import { useRef, } from "react";
+
+
+
+
 
  const Contact =() => {
 
     const form = useRef();
 
     const sendEmail = (e) => {
-        e.preventDefault();
+        e.preventDefault()
      
         emailjs.sendForm(
-            "service_33jp94m",
-            "template_mr4kv6s",
+            "service_kz4qfkb",
+            "service_kz4qfkb",
             form.current,
-            "user_SO4fmcQs-RyLpYi3Z"
-        ); // erro
+            "user_XAFCngR21pAvnH2rP"
+        ) // erro
     };
+
+
+
     return (
         <section id="contact">
             <h5>Get In Touch</h5>
             <h2>Contact Me</h2>
+            
             <div className="container contact__container">
                 <div className="contact__options">
                     <article className="contact__option">
                         <MdOutlineEmail className="contact__option-icon" />
                         <h4>Email</h4>
-                        <h5>dummyegator@gmail.com</h5>
-                        <a href="joao4729@gmail.com" target="__blank">
-                            Send message
+                        <h5>joao4729@gmail.com</h5>
+                        <a href="mailto:joao4729@gmail.com" target="__blank">
+                            Send a message
                         </a>
                     </article>
 
                     <article className="contact__option">
                         <RiMessengerLine className="contact__option-icon" />
                         <h4>Messager</h4>
-                        <h5>egatortutorials</h5>
+                        <h5>JB</h5>
                         <a href="http://m.me/ernest.achievier" target="__blank">
                             Send message
                         </a>
@@ -47,18 +55,18 @@ import { useRef, } from "react";
                     <article className="contact__option">
                         <BsWhatsapp className="contact__option-icon" />
                         <h4>WhatssApp</h4>
-                        <h5>+123456789</h5>
-                        <a href="https://api.WhatsApp.com/send?phone+55-319966567563" target="__blank">
+                        <h5>55+31996647563</h5>
+                        <a href="https://api.WhatsApp.com/55+31996647563" target="__blank">
                             Send message
                         </a>
                     </article>
                 </div>
                 {/*=================================END CONTACT OPTION============*/}
                 <form ref={form} onSubmit={sendEmail}>
-                    
+                   
                        <input type="text" name="name"  placeholder="Your Full Name" required />
                        <input type="email" name="email" placeholder="Your Email" required />
-                       <textarea name="message"rows="7" placeholder="Your Message"required ></textarea>
+                       <textarea name="message"rows="7" placeholder="Your Message"required></textarea>
                        <button type="submit" className="btn btn-primary">Send Message</button>
                 </form>
             </div>
@@ -66,6 +74,9 @@ import { useRef, } from "react";
     );
 }
 export default Contact
+                    
+                    
+                     
 
   
 
